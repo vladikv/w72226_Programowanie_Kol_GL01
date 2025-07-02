87991710f0fb3b0a6ed5d08eca11a142fbebb055
Vladyslav Vasyliv <w72226@student.wsiz.edu.pl>
2025-07-02 10:37:06 +0200

    M

--- Zadanie_3/main.js
+++ Zadanie_3/main.js
@@ -1,5 +1,5 @@
 document.getElementById('registerForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // zatrzymuje domyślną akcję formularza
+    e.preventDefault(); 
 
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value;
